0e9c6e1e6a4b05eadce53301d09e490ec0d71261
CarlosArch <carlos_arch@hotmail.com>
2020-06-08 14:55:52 -0500

    World transfer feedback
    Now succesfully returns stdout/stderr to a textbox

--- core/application.py
+++ core/application.py
@@ -21,6 +21,7 @@ class HomePage(Page):
         ttk.Button(frame,
                    text='Open SSH to server',
                    command=self.ssh_to_server).pack()
+        frame.config(padding=10)
         return frame
 
     def ssh_to_server(self):
@@ -35,42 +36,61 @@ class WorldTransferPage(Page):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.subprocess = Subprocess(self.update)
-
-    def update(self, queue):
-        if not queue.empty():
-            output, error = queue.get()
-            output = output.strip()
-            error = output.strip()
-
-            self.txt_progress_box.configure(state='normal')
-
-            self.txt_progress_box.delete(0, 'end')
-            if error:
-                self.txt_progress_box.insert(0, error, 'ERR')
-            elif output:
-                self.txt_progress_box.insert(0, output)
-
-            self.txt_progress_box.configure(state='disabled')
-            queue.task_done()
+        self.subprocess = Subprocess(self.update_textbox, delay_s=1)
 
     def make_content(self, frame):
         self.inp_world_name = TextInput(frame,
                                         label_text='World Name',
                                         default=CONFIG.get('world',
                                                            'world_name'))
-        self.inp_world_name.grid()
+        self.inp_world_name.grid(columnspan=2)
         ttk.Button(frame,
                    text='Send world',
-                   command=self.send_world).grid(row=1)
+                   command=self.send_world).grid(row=1, column=0,
+                                                 sticky='ew')
         ttk.Button(frame,
                    text='Retrieve world',
-                   command=self.retrieve_world).grid(row=2)
+                   command=self.retrieve_world).grid(row=1, column=1,
+                                                     sticky='ew')
 
-        self.txt_progress_box = tk.Text(frame, height=1 ,state='disabled')
+        self.txt_progress_box = tk.Text(frame,
+                                        height=1,
+                                        width=1,
+                                        state='disabled')
         self.txt_progress_box.tag_configure('ERR', foreground='red')
-        self.txt_progress_box.grid(row=3, sticky='ew')
+        self.txt_progress_box.tag_configure('OUT', foreground='black')
+        self.txt_progress_box.grid(row=3, columnspan=2, sticky='ew')
+
+        frame.grid_columnconfigure(0, weight=1)
+        frame.grid_columnconfigure(1, weight=1)
+        frame.config(padding=10)
         return frame
+
+    def update_textbox(self, queue):
+        if not queue.empty():
+            output, error, returncode = queue.get()
+            output = output.strip()
+            error = error.strip()
+
+            self.txt_progress_box.configure(state='normal')
+            self.txt_progress_box.delete('0.0', 'end')
+            if error:
+                text = (error, 'ERR')
+            else:
+                text = (output, 'ERR')
+            self.txt_progress_box.insert('0.0', *text)
+            while True:
+                end_char = f'{self.txt_progress_box.index("end")} - 1 chars'
+                end_visible = self.txt_progress_box.bbox(end_char) is not None
+                current_height = self.txt_progress_box.cget('height')
+
+                if not end_visible:
+                    self.txt_progress_box.configure(height=current_height+1)
+                    self.master.show_page(self.__class__)
+                else:
+                    break
+            self.txt_progress_box.configure(state='disabled')
+            queue.task_done()
 
     def send_world(self):
         server_dns, world_name = self.world_config()
@@ -99,15 +119,17 @@ class WorldTransferPage(Page):
     def _secure_copy(self, file_from, file_to):
         key_path = CONFIG.get('server', 'key_path')
         command = ['scp',
-                   '-i', f'config/{key_path}',
+                   f'-i config/{key_path}',
                    f'{file_from}',
                    f'{file_to}']
+        # command = ['scp',
+        #            f'scp_testerino/input/scp_test.txt',
+        #            f'scp_testerino/output/']
         self.subprocess.communicate(command)
 
 
 class ServerSettingsPage(Page):
     title = 'Server Settings'
-
     def make_content(self, frame):
         self.form = ServerSettingsForm(frame)
         self.form.pack(fill='both', expand=True, padx=10, pady=10)
